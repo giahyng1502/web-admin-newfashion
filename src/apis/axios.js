f4b9510ff3a65ecef772c3ddb9d631c96f7185f8
hungcy <hungcy1502@gmail.com>
2025-05-04 00:53:25 +0700

    update baseUrl

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: 'https://api.hungcy.id.vn/'
 });
 
 // Thêm interceptor để tự động chèn token vào mỗi reque
